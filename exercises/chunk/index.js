4dd826dfa9104ea02b8fe02bad286a2f1ed988e9
Tam Phan <tnphan0204@gmail.com>
2019-11-15 23:24:07 -0500

    solved chunk arrays

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    //create empty array to hold chunks(sub arrays)
+    //for each element of the 'unchunked' array called 'chunked'
+    //retrieve last element(array) in 'chunked'
+    //IF last element does not exist, or if its length is equal to chunk size
+        //push new chunk into 'chunked'
+    //ELSE add current element into the chunk
+    let chunked = [];
+    for (let i = 1; i <= array.length; i++) {
+        const last = chunked[chunked.length - 1]
+
+        if (!last || last.length === size) {
+            chunked.push([i])
+        } else {
+            last.push(i)
+        }
+    }
+    return chunked;
+}
 
 module.exports = chunk;
